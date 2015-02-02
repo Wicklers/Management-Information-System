8af246a07195fe15ad3943b9ef3c6c5059b976ab
Harsh Vardhan Ladha <harsh.ladha@gmail.com>
2015-02-02 15:37:55 +0530

    Full configuration file!

--- core/config_sample.php
+++ core/config_sample.php
@@ -1,40 +1,68 @@
 <?php
 
 /**
+ * 
+ *
  * @author Harsh Vardhan Ladha & Yogesh Chauhan
+ * @author 2Wicklers
  * @version 1.0
  * @copyright Computer Science & Engineering Department, NIT Silchar 
  * @package MIS
- * @link http://nits.ac.in
+ * @link http://mis.nits.ac.in
+ * @license NIT Silchar
  */
  
 
 /**
  * MySQL Database Server Address
  */
-define("DB_SERVER","db-server");
+define("DB_SERVER","localhost");
 
 
 
 /**
  * MySQL Database Username
  */
-define("DB_USERNAME","db-username");
+define("DB_USERNAME","root");
 
 /**
  * MySQL Database Password
  */
-define("DB_PASSWORD","db-password");
+define("DB_PASSWORD","*******");
 
 /**
  * DATABASE NAME of the System
  */
-define("DB_NAME","db-name");
+define("DB_NAME","mis");
 
  /**
  * LDAP_SERVER location or Address
  */
-define("LDAP_SERVER","ldap://ldap-server");
+define("LDAP_SERVER","ldap://172.161.231.70");
+
+/**
+ * LDAP_SUPERUSER_PASSWORD is a password used for searching in LDAP directory
+ */
+define("LDAP_SUPERUSER_PASSWORD","*******");
+
+/**
+ * LDAP_SUPERUSER is DN used to search in LDAP directory
+ */
+define("LDAP_SUPERUSER", "uid=*****,cn=*****,cn=****");
+
+
+
+/**
+* BIZGROW SMS API KEY
+*/
+define("SMS_APIKEY", "***************");
+
+/**
+* SMS SENDER ID
+*/
+define("SMS_SENDER_ID", "NITSMS");
+
+
 
 header("Cache-Control: no-cache");
 header("Pragma: no-cache");
@@ -42,4 +70,8 @@ header("Pragma: no-cache");
 //ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+/**
+ * Setting timezone for India :) 
+ */
+date_default_timezone_set('Asia/Kolkata');
 ?>
